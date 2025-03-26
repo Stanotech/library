ae3d2bdf29120f16327f9b0c5fdb5672ed1a3370
Stanotech <satach@o2.pl>
2025-03-26 23:18:39 +0100

    get response status

--- app/library/views.py
+++ app/library/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from library.models import Book
 from library.serializers import BookSerializer
 
 class LibraryView(ApiView):
     def get(self, request):
         books = Book.objects.all()
-        return Response({"books": books})
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
 class BookDetailView(ApiView):
     def get(self, request, book_id):
         book = Book.objects.get(id=book_id)
         serializer = BookSerializer(book)
-        return Response(serializer.data, content_type="application/json")
+        return Response(serializer.data, status=status.HTTP_200_OK)
