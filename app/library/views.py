bcfa134f631fd25c4abcb7b09f28e2dece59ddab
Stanotech <satach@o2.pl>
2025-03-26 23:32:05 +0100

    post method

--- app/library/views.py
+++ app/library/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import 
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+
 from library.models import Book
 from library.serializers import BookSerializer
 
-class LibraryView(ApiView):
+class LibraryView(APIView):
     def get(self, request):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-class BookDetailView(ApiView):
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+            except Exception as e:
+                return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class BookDetailView(APIView):
     def get(self, request, book_id):
         book = Book.objects.get(id=book_id)
         serializer = BookSerializer(book)
