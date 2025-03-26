2d0c8278531ee28aa4c4a6a8d6bdae17796d37b2
Stanotech <satach@o2.pl>
2025-03-26 22:55:55 +0100

    serializer and BookDetailView

--- app/library/urls.py
+++ app/library/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LibraryView
+
+from library.views import LibraryView, BookDetailView
 
 urlpatterns = [
     path("books", LibraryView.as_view(), name="book-list"),
-    path("book/<int:book_id>", LibraryView.as_view(), name="book-detail")
+    path("book/<int:book_id>", BookDetailView.as_view(), name="book-detail")
 ]
--- app/library/views.py
+++ app/library/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from library.models import Book
+from library.serializers import BookSerializer
 
 class LibraryView(ApiView):
     def get(self, request):
         books = Book.objects.all()
         return Response({"books": books})
+    
+class BookDetailView(ApiView):
+    def get(self, request, book_id):
+        book = Book.objects.get(id=book_id)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, content_type="application/json")
